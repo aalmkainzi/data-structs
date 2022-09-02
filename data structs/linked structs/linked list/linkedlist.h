dd163fbbdd430e451ac85bf3ddbf2bb118b3b7b2
Abdulmalek Almkainzi <malekwryyy@hotmail.com>
2022-09-03 00:15:35 +0300

    Update linkedlist.h

--- data structs/linked structs/linked list/linkedlist.h
+++ data structs/linked structs/linked list/linkedlist.h
@@ -1,5 +1,5 @@
-#ifndef CPUSCHEDULERSIMULATOR_LINKEDLIST_H
-#define CPUSCHEDULERSIMULATOR_LINKEDLIST_H
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
 
 #include <corecrt.h>
 #include <stdbool.h>
@@ -24,8 +24,8 @@ typedef struct
     linked_metadata metadata;
 } linkedlist;
 
-//make arg[0] NULL if you don't plan on using ls_contains, ls_delete_element, or ls_get_data.
-//If you do, either initialize equals in this init function or by assigning to ls->metadata.equals afterwards
+//make arg[0] NULL if you won't use ls_contains, ls_delete_element, or ls_get_data.
+//If you will, either initialize equals in this init function or by assigning to ls->metadata.equals afterwards
 linkedlist* init_linkedlist(bool (*equals)(const void*, const void*));
 void ls_add(linkedlist*, void*);
 void* ls_delete_at(linkedlist*, int);
